1740858697e882960a9af130f46f7210de7519bf
Franz R <franz.reitmayer@reitmayer.eu>
2021-06-28 09:31:40 +0000

    initial commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("eu.reitmayer.test.apps.ztstx02.controller.View1", {
+			onInit: function () {
+
+			}
+		});
+	});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"eureitmayer.test.apps./ztstx02/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
